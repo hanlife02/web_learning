f5d76e1329a19dc68b3ce1f79db1e601f8ff8457
Ethan <yanghan0911@gmail.com>
2025-04-04 14:01:25 +0800

    re hello.js

---
+++ hello.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: Ethan && ethan@hanlife02.com
+ * @Date: 2025-04-04 13:39:48
+ * @LastEditors: Ethan && ethan@hanlife02.com
+ * @LastEditTime: 2025-04-04 14:00:45
+ * @FilePath: /code/web/hello.js
+ * @Description:
+ *
+ * Copyright (c) 2025 by Ethan, All Rights Reserved.
+ */
+
+// const arrCtoF = (arrC) => {
+//     const arrF = [];
+//     for(let i = 0;i < arrC.length;i++){
+//         arrF.push(arrF[i] * 9/5 + 32);
+//     }
+//     return arrF;
+// };
+
+// const array = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// const newarray = array.map((item) => {
+//   return item / 10;
+// });
+
+// console.log(newarray);
